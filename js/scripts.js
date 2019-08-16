692e86d523ea47aa45209b8f16a26510d46cc7af
Student <example@example.com>
2019-08-16 16:53:48 -0700

    study for weekTest

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+
+$(function() {
+  $("button#h").click(function() 
+}
+
+var survey =$(input:radio[name=surbey]: checked").val();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
